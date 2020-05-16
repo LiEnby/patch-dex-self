d6f0a8b96411994ea36ad9a53d85b5ab79c80a6e
Bluzume <39113159+KuromeSan@users.noreply.github.com>
2020-05-16 14:03:40 +1200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	unsigned long long AuthID = 0x00;
+
+	if (argc == 1)
+	{
+		printf("SELF not specified in arg1");
+		return 1;
+	}
+	else
+	{
+		FILE* fd = fopen(argv[1],"r+");
+		if (fd == NULL)
+		{
+			printf("File not found %s",argv[1]);
+			return 1;
+		}
+		fseek(fd,0x80,SEEK_SET);
+		fread(&AuthID, sizeof(unsigned long long), 1, fd);
+		printf("Current Authid: %llx\n", AuthID);
+		if (AuthID == 0x2f00000000000001)
+		{
+			AuthID = 0x2f00000000000101;
+			fseek(fd, 0x80, SEEK_SET);
+			fwrite(&AuthID, sizeof(unsigned long long), 1, fd);
+			printf("New Authid: %llx\n", AuthID);
+		}
+		else
+		{
+			printf("Allready works on activated dev/test kit.\n");
+		}
+		fclose(fd);
+		return 0;
+	}
+}
